a35bb47770d8fd8d5598ed6099470f22bd32dffe
Roman <rommmmmashka@gmail.com>
2026-03-12 20:41:14 +0200

    lab4

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Profile, Item, Achievement
 
 # Register your models here.
+#admin.site.register(Profile)
+#admin.site.register(Item)
+#admin.site.register(Achievement)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("name","level","created_at", "updated_at")
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name","item_type", "created_at", "updated_at")
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ("title","created_at", "updated_at")
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Achievement, AchievementAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=40)
+    level = models.IntegerField(default=1)
+    achievements = models.ManyToManyField("Achievement")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Achievement(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    item_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
